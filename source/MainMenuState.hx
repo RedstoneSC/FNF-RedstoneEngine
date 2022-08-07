aff0d2628ebab5821769278cb62bc3255933a225
Gabriel2019r <deltarune.ultimate.rp@gmail.com>
2022-08-07 10:48:43 +0200

    Fixing

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var gabVersion:String = '0.1.2'; //This is also used for Discord RPC
+	public static var psychEngineVersion:String = '0.1.2'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
@@ -127,7 +127,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Gab Engine v" + gabVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Gab Engine v" + psychEngineVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("Permanent Marker Regular", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
