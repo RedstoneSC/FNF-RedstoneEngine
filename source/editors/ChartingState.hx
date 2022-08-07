8f326127f1ea630b064defc7057d1b8311cf4ecd
Gabriel2019r <deltarune.ultimate.rp@gmail.com>
2022-08-07 14:45:55 +0200

    bruh

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -86,6 +86,7 @@ class ChartingState extends MusicBeatState
 		['Screen Shake', "Value 1: Camera shake\nValue 2: HUD shake\n\nEvery value works as the following example: \"1, 0.05\".\nThe first number (1) is the duration.\nThe second number (0.05) is the intensity."],
 		['Change Character', "Value 1: Character to change (Dad, BF, GF)\nValue 2: New character's name"],
 		['Change Scroll Speed', "Value 1: Scroll Speed Multiplier (1 is default)\nValue 2: Time it takes to change fully in seconds."]
+		['Rainbow Eyesore', "Value 1: Starting duration\nValue 2: End of duration."]
 	];
 
 	var _file:FileReference;
