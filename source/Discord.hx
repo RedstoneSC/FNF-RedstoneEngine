e6da1c0429988c8b4540f40aedefa92ba453b10b
Gabriel2019r <deltarune.ultimate.rp@gmail.com>
2022-08-08 20:22:21 +0200

    Gab Engine 0.1.3 thing

--- source/Discord.hx
+++ source/Discord.hx
@@ -17,7 +17,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "863222024192262205",
+			clientID: "1006257730727710741",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
@@ -45,7 +45,7 @@ class DiscordClient
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "Psych Engine"
+			largeImageText: "Gab Engine"
 		});
 	}
 
