512742aec904524b61a21550e13df8aa2653eb59
Gabriel2019r <deltarune.ultimate.rp@gmail.com>
2022-10-02 17:09:03 +0200

    will this even work

--- source/options/NoteSkinState.hx
+++ source/options/NoteSkinState.hx
@@ -45,7 +45,7 @@ class NoteSkinState extends MusicBeatState {
         grpNotes = new FlxTypedGroup<FlxSprite>();
         add(grpNotes);
 
-        var skinPath:String = 'noteSkins/';
+        var skinPath:String = 'assets/shared/images/noteSkins/';
 
         if(FileSystem.exists(skinPath)) {
             trace("Note skins found");
