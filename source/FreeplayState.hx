fc49aac7fc425e04c0c4c352823f7ebe19b82115
GabTheToastyBuns <100803757+Gabriel2019r@users.noreply.github.com>
2022-10-24 11:25:46 +0200

    bruh

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -21,6 +21,9 @@ import WeekData;
 #if MODS_ALLOWED
 import sys.FileSystem;
 #end
+	
+import flixel.util.FlxGradient;
+// credits to DavidDX on gradient
 
 using StringTools;
 
@@ -51,6 +54,8 @@ class FreeplayState extends MusicBeatState
 	var colorTween:FlxTween;
 
 	var repText:FlxText;
+
+	var leGradientBar:FlxSprite = new FlxSprite(0, 0).makeGraphic(FlxG.width, 300, 0xFFAA00AA);
 	
 	override function create()
 	{
@@ -65,7 +70,7 @@ class FreeplayState extends MusicBeatState
 		// Updating Discord Rich Presence
 		DiscordClient.changePresence("In the Menus", null);
 		#end
-
+			
 		for (i in 0...WeekData.weeksList.length) {
 			if(weekIsLocked(WeekData.weeksList[i])) continue;
 
@@ -107,6 +112,11 @@ class FreeplayState extends MusicBeatState
 		bg.antialiasing = ClientPrefs.globalAntialiasing;
 		add(bg);
 		bg.screenCenter();
+		
+		leGradientBar = FlxGradient.createGradientFlxSprite(Math.round(FlxG.width), 512, [0x00ff0000, 0x55AE59E4, 0xAA19ECFF], 1, 90, true);
+		leGradientBar.y = FlxG.height - leGradientBar.height;
+		add(leGradientBar);
+		leGradientBar.scrollFactor.set(0, 0);
 
 		grpSongs = new FlxTypedGroup<Alphabet>();
 		add(grpSongs);
@@ -149,7 +159,7 @@ class FreeplayState extends MusicBeatState
 		scoreText = new FlxText(FlxG.width * 0.7, 5, 0, "", 32);
 		scoreText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, RIGHT);
 
-		scoreBG = new FlxSprite(scoreText.x - 6, 0).makeGraphic(1, 66, 0xFF000000);
+		scoreBG = new FlxSprite(scoreText.x - 6, 0).makeGraphic(1, 92, 0xFF000000);
 		scoreBG.alpha = 0.6;
 		add(scoreBG);
 
@@ -163,7 +173,7 @@ class FreeplayState extends MusicBeatState
 		repText.text = 'Press ALT to show the replays of ${songs[curSelected].songName.toUpperCase()}';
 		repText.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, RIGHT);
 		add(repText);
-		
+
 		if(curSelected >= songs.length) curSelected = 0;
 		bg.color = songs[curSelected].color;
 		intendedColor = bg.color;
@@ -281,6 +291,7 @@ class FreeplayState extends MusicBeatState
 		var accepted = controls.ACCEPT;
 		var space = FlxG.keys.justPressed.SPACE;
 		var ctrl = FlxG.keys.justPressed.CONTROL;
+		var alt = FlxG.keys.justPressed.ALT;
 
 		var shiftMult:Int = 1;
 		if(FlxG.keys.pressed.SHIFT) shiftMult = 3;
@@ -414,8 +425,6 @@ class FreeplayState extends MusicBeatState
 
 	function changeDiff(change:Int = 0)
 	{
-		repText.text = 'Press ALT to show the replays of ${songs[curSelected].songName.toUpperCase()}';
-		
 		curDifficulty += change;
 
 		if (curDifficulty < 0)
@@ -437,6 +446,8 @@ class FreeplayState extends MusicBeatState
 
 	function changeSelection(change:Int = 0, playSound:Bool = true)
 	{
+		repText.text = 'Press ALT to show the replays of ${songs[curSelected].songName.toUpperCase()}';
+
 		if(playSound) FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
 
 		curSelected += change;
